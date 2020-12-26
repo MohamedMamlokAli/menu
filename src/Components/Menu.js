0b8f86bd105991a3c9a39694bc9eb950405fa51b
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2020-12-26 13:25:34 +0200

    Added data

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Menu = () => {
+  const [all, setAll] = useState(true);
+  const [breakfast, setBreakfast] = useState(false);
+  const [lunch, setLunch] = useState(false);
+  const [shakes, setShakes] = useState(false);
   return <div>hello world</div>;
 };
 
