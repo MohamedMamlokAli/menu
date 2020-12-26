248e5113b037068a3845b02c324211c425b9d979
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2020-12-26 14:29:51 +0200

    Finished functionality , remaining CSS

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,127 @@
 import React, { useState } from "react";
+import { data } from "./data";
 
 const Menu = () => {
   const [all, setAll] = useState(true);
   const [breakfast, setBreakfast] = useState(false);
   const [lunch, setLunch] = useState(false);
   const [shakes, setShakes] = useState(false);
-  return <div>hello world</div>;
+
+  return (
+    <>
+      <h2>Our Menu</h2>
+      <div className="container">
+        <div className="containerNav">
+          <button
+            onClick={() => {
+              setAll(true);
+              setBreakfast(false);
+              setLunch(false);
+              setShakes(false);
+            }}
+          >
+            All
+          </button>
+          <button
+            onClick={() => {
+              setAll(false);
+              setBreakfast(true);
+              setLunch(false);
+              setShakes(false);
+            }}
+          >
+            Breakfast
+          </button>
+          <button
+            onClick={() => {
+              setAll(false);
+              setBreakfast(false);
+              setLunch(true);
+              setShakes(false);
+            }}
+          >
+            Lunch
+          </button>
+          <button
+            onClick={() => {
+              setAll(false);
+              setBreakfast(false);
+              setLunch(false);
+              setShakes(true);
+            }}
+          >
+            Shakes
+          </button>
+        </div>
+        {all && (
+          <div className="menu">
+            {data.map((food) => {
+              const { id, img, name, price, description } = food;
+              return (
+                <div className="food" key={id}>
+                  <img src={img} alt="" />
+                  <h4>{name}</h4>
+                  <p>{price}</p>
+                  <p>{description}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {lunch && (
+          <div className="menu">
+            {data.map((food) => {
+              if (food.catagory == "lunch") {
+                const { id, img, name, price, description } = food;
+                return (
+                  <div className="food" key={id}>
+                    <img src={img} alt="" />
+                    <h4>{name}</h4>
+                    <p>{price}</p>
+                    <p>{description}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
+        {shakes && (
+          <div className="menu">
+            {data.map((food) => {
+              if (food.catagory == "shake") {
+                const { id, img, name, price, description } = food;
+                return (
+                  <div className="food" key={id}>
+                    <img src={img} alt="" />
+                    <h4>{name}</h4>
+                    <p>{price}</p>
+                    <p>{description}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
+        {breakfast && (
+          <div className="menu">
+            {data.map((food) => {
+              if (food.catagory == "breakfast") {
+                const { id, img, name, price, description } = food;
+                return (
+                  <div className="food" key={id}>
+                    <img src={img} alt="" />
+                    <h4>{name}</h4>
+                    <p>{price}</p>
+                    <p>{description}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Menu;
--- src/Components/data.js
+++ src/Components/data.js
@@ -2,74 +2,83 @@ export const data = [
   {
     id: "1",
     img: "https://react-projects-5-menu.netlify.app/images/item-1.jpeg",
-    food: "Buttermilk Pancakes",
+    name: "Buttermilk Pancakes",
     price: "$15",
     description: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`,
+    catagory: "breakfast",
   },
   {
     id: "2",
     img: "https://react-projects-5-menu.netlify.app/images/item-2.jpeg",
-    food: "Diner Double",
+    name: "Diner Double",
     price: "$15",
     description: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats`,
+    catagory: "lunch",
   },
   {
     id: "3",
     img: "https://react-projects-5-menu.netlify.app/images/item-3.jpeg",
-    food: "Godzilla Milkshake",
+    name: "Godzilla Milkshake",
     price: "$15",
     description: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.
 
 `,
+    catagory: "shake",
   },
   {
     id: "4",
     img: "https://react-projects-5-menu.netlify.app/images/item-4.jpeg",
-    food: "Country Delight",
+    name: "Country Delight",
     price: "$15",
     description: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,
 
 `,
+    catagory: "breakfast",
   },
   {
     id: "5",
     img: "https://react-projects-5-menu.netlify.app/images/item-5.jpeg",
-    food: "Egg Attack",
+    name: "Egg Attack",
     price: "$15",
     description: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up
 
 `,
+    catagory: "lunch",
   },
   {
     id: "6",
     img: "https://react-projects-5-menu.netlify.app/images/item-6.jpeg",
-    food: "Oreo Dream",
+    name: "Oreo Dream",
     price: "$15",
     description: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday
 `,
+    catagory: "shake",
   },
   {
     id: "7",
     img: "https://react-projects-5-menu.netlify.app/images/item-7.jpeg",
-    food: "Bacon Overflow",
+    name: "Bacon Overflow",
     price: "$15",
     description: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird
 
 `,
+    catagory: "breakfast",
   },
   {
     id: "8",
     img: "https://react-projects-5-menu.netlify.app/images/item-8.jpeg",
-    food: "American Classic",
+    name: "American Classic",
     price: "$15",
     description: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut
 `,
+    catagory: "lunch",
   },
   {
     id: "9",
     img: "https://react-projects-5-menu.netlify.app/images/item-9.jpeg",
-    food: "Quarantine Buddy",
+    name: "Quarantine Buddy",
     price: "$15",
     description: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    catagory: "shake",
   },
 ];
